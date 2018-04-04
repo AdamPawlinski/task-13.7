58267bf428ed5f1da10f4698842515d34f75db3b
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-04 21:02:20 +0200

    finished but still not working, file shows undefined

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+process.stdin.on('readable', function() {
+  var path = process.stdin.read();
+  if (path !== null) {
+    var dirPath = path.toString().trim();
+    fs.readdir(dirPath, 'utf-8', function(err, files){
+      fs.writeFile('./dirFiles.txt', files, function(err) {
+        if (err) throw err;
+        console.log('Filenames from directory ' + dirPath + ' were written.');
+      });
+    });
+  }
+});
